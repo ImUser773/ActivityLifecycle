dadd63531306b073d6cf89c6e59f7e793d9c5b59
IamDeveloper <iauser773@gmail.com>
2016-09-09 23:31:30 +0700

    Activity Lifecycle

---
+++ app/src/main/java/com/iamdeveloper/applicationlifecycle/MainActivity.java
@@ -0,0 +1,51 @@
+package com.iamdeveloper.applicationlifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("LifeCycle","onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("LifeCycle","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("LifeCycle","onResume");
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("LifeCycle","onPause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("LifeCycle","onStop");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("LifeCycle","onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("LifeCycle","onDestroy");
+    }
+}
